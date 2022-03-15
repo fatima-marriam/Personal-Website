9da3069292b10b44e5c4e4b4a249fe35282c8abb
fatima-marriam <fatimamarriam842@gmail.com>
2022-03-16 00:19:42 +0500

    update testimonial section

--- main.js
+++ main.js
@@ -62,6 +62,24 @@ function activeWork(){
     this.classList.add("active-work")
 }
 linkWork.forEach(l=> l.addEventListener('click', activeWork)) 
-
+// =======================swiper testimonial
+var swiperTestimonial = new Swiper(".testimonial__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+        },
+      },
+  });
 
 
